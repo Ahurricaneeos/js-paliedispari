ccc73c229c87d1f7b342d5ac9e8035a680288edd
Ahurricaneeos <aurora_sala@hotmail.it>
2023-04-05 17:39:55 +0200

    Added validate input

--- paridispari/script.js
+++ paridispari/script.js
@@ -1,8 +1,9 @@
 let userChoiceEvenOdd = prompt("Pari o Dispari?");
+let userEvenOdd = validateInputEvenOdd(userChoiceEvenOdd);
 let userChoiceNumber = prompt("Inserisci un numero da 1 a 5:");
 let userNumber = validateInputNumber(userChoiceNumber);
 let computerChoice = randomNumberGenerator();
-let sumNumber = evenOddNumbersSum();
+let sumNumber = evenOddNumbersSum(userNumber, computerChoice);
 
 
 // funzioni
@@ -16,18 +17,24 @@ function validateInputNumber(stringa) {
     return numero;
 }
 
+function validateInputEvenOdd(stringa) {
+    while(stringa !== "pari" && stringa !== "dispari"){
+        stringa = prompt("Scrivi 'pari' o 'dispari'");
+    }
+}
+
 function randomNumberGenerator(computerChoice) {
     computerChoice = Math.floor(Math.random() * 5);
     return computerChoice;
 }
 
 function evenOddNumbersSum(userNumber, computerChoice) {
-    const sumNumber = userNumber+computerChoice;
+    let sumNumber = userNumber+computerChoice;
     if(sumNumber % 2 == 0){
-      console.log("La somma dei numeri è:" + sumNumber +" - pari");
+      console.log("La somma dei numeri è: " + sumNumber +" - pari");
       return sumNumber;
     } else {
-    console.log("La somma dei numeri è:" + sumNumber +" - dispari");
+    console.log("La somma dei numeri è: " + sumNumber +" - dispari");
     return sumNumber;
     }
 }
